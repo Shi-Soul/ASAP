d8668c924985dad392fb76cd43b74fc3262edebb
Shi-Soul <993520026@qq.com>
2025-03-12 17:35:07 +0800

    .

---
+++ humanoidverse/deploy/mujoco.py
@@ -0,0 +1,290 @@
+# Unified Robot Control Interface for HumanoidVerse
+# Weiji Xie @ 2025.03.04
+
+import mujoco, mujoco_viewer
+import glfw 
+import os
+import sys
+from pathlib import Path
+
+import torch
+from humanoidverse.deploy import URCIRobot
+from scipy.spatial.transform import Rotation as R
+import logging
+from utils.config_utils import *  # noqa: E402, F403
+# add argparse arguments
+
+from typing import Dict, Optional
+from humanoidverse.utils.config_utils import *  # noqa: E402, F403
+from loguru import logger
+
+
+import numpy as np
+np2torch = lambda x: torch.tensor(x, dtype=torch.float32)
+torch2np = lambda x: x.cpu().numpy()
+
+def wrap_to_pi_float(angles:float):
+    angles %= 2*np.pi
+    angles -= 2*np.pi * (angles > np.pi)
+    return angles
+
+def quaternion_to_euler_array(quat):
+    # Ensure quaternion is in the correct format [x, y, z, w]
+    x, y, z, w = quat
+    
+    # Roll (x-axis rotation)
+    t0 = +2.0 * (w * x + y * z)
+    t1 = +1.0 - 2.0 * (x * x + y * y)
+    roll_x = np.arctan2(t0, t1)
+    
+    # Pitch (y-axis rotation)
+    t2 = +2.0 * (w * y - z * x)
+    t2 = np.clip(t2, -1.0, 1.0)
+    pitch_y = np.arcsin(t2)
+    
+    # Yaw (z-axis rotation)
+    t3 = +2.0 * (w * z + x * y)
+    t4 = +1.0 - 2.0 * (y * y + z * z)
+    yaw_z = np.arctan2(t3, t4)
+    
+    # Returns roll, pitch, yaw in a NumPy array in radians
+    return np.array([roll_x, pitch_y, yaw_z])
+
+
+class MujocoRobot(URCIRobot):
+    REAL=False
+    
+    RAND_NOISE: bool = True
+    RAND_DELAY: bool = True
+    RAND_MASK : bool = True
+    
+    noise_ratio = 3e-2
+    # delay_ratio = (4, 25) # unit: ms
+    delay_ratio = (4, 20) # unit: ms
+    mask_ratio = 0.7
+    mk_rand_noise = lambda tens, ratio: (
+                                            (tens * (1 + torch.randn_like(tens) * ratio) ) 
+                                    if isinstance(tens, torch.Tensor) 
+                                    else    (tens * (1 + np.random.randn(*tens.shape).astype(tens.dtype) * ratio)) )# type: ignore
+    # mk_rand_noise = lambda tens, ratio: tens * (1 + np.random.randn(*tens.shape).astype(tens.dtype) * ratio) # type: ignore
+    
+    print_torque = lambda tau: print(f"tau (norm, max) = {np.linalg.norm(tau):.2f}, \t{np.max(tau):.2f}", end='\r')
+    
+    def __init__(self, cfg):
+        super().__init__(cfg)
+        
+        self.decimation = cfg.simulator.config.sim.control_decimation
+        self.sim_dt = 1/cfg.simulator.config.sim.fps
+        assert self.dt == self.decimation * self.sim_dt
+        # self._subtimer = 0
+        
+        
+        
+        self.model = mujoco.MjModel.from_xml_path(os.path.join(cfg.robot.asset.asset_root, cfg.robot.asset.xml_file)) # type: ignore
+        # self.model = mujoco.MjModel.from_xml_path('/home/bai/ASAP/humanoidverse/data/robots/g1/g1_23dof_lock_wrist_phys.xml')
+        self.data = mujoco.MjData(self.model) # type: ignore
+        self.model.opt.timestep = self.sim_dt
+        if cfg.deploy.render:
+            self.is_render = True
+            self.__make_viewer()
+        
+        self.num_ctrl = self.data.ctrl.shape[0]
+        assert self.num_ctrl == self.num_actions, f"Number of control DOFs {self.num_ctrl} does not match number of actions {self.num_actions}"
+        
+        logger.info("Initializing Mujoco Robot")
+        logger.info("Task Name: {}".format(cfg.log_task_name))
+        logger.info("Robot Type: {}".format(cfg.robot.asset.robot_type))
+        
+        
+        self.Reset()
+        
+        mujoco.mj_step(self.model, self.data) # type: ignore    
+
+    # TODO: visualize the motion keypoint in MujocoViewer
+    def __make_viewer(self):
+        ...
+        self.viewer = mujoco_viewer.MujocoViewer(self.model, self.data)
+        self.viewer.cam.lookat[:] = np.array([0,0,0.8])
+        self.viewer.cam.distance = 3.0        
+        self.viewer.cam.azimuth = 30                         # 可根据需要调整角度
+        self.viewer.cam.elevation = -30                      # 负值表示从上往下看
+        def _key_callback(window, key, scancode, action, mods):
+            if action == glfw.PRESS:
+                #  Keyboard mapping:
+                #  ----------------
+                #  |   K L ; '    |
+                #  |   , . /      |
+                #  ----------------
+
+                if key == glfw.KEY_COMMA:
+                    self.cmd[1] += 0.1
+                elif key == glfw.KEY_SLASH:
+                    self.cmd[1] -= 0.1
+                elif key == glfw.KEY_L: #
+                    self.cmd[0] += 0.1
+                elif key == glfw.KEY_PERIOD:
+                    self.cmd[0] -= 0.1
+                elif key == glfw.KEY_K:
+                    if self.heading_cmd:
+                        self.cmd[3] = wrap_to_pi_float(self.cmd[3]+np.pi/20)
+                    else:
+                        self.cmd[2] += 0.1
+                elif key == glfw.KEY_SEMICOLON:
+                    if self.heading_cmd:                            
+                        self.cmd[3] = wrap_to_pi_float(self.cmd[3]-np.pi/20)
+                    else:
+                        self.cmd[2] -= 0.1
+                elif key == glfw.KEY_APOSTROPHE:
+                    self.cmd = np.array(self.cfg.deploy.defcmd)
+                elif key == glfw.KEY_ENTER:
+                    self.Reset()
+                elif key == glfw.KEY_LEFT_BRACKET:
+                    self._ref_pid -= 1
+                elif key == glfw.KEY_RIGHT_BRACKET:
+                    self._ref_pid += 1
+                
+                print(self.cmd)
+            self.viewer._key_callback(window, key, scancode, action, mods)
+        glfw.set_key_callback(self.viewer.window, _key_callback)
+
+    def Reset(self):
+        # raise NotImplementedError("Not implemented")
+        self.data.qpos[:3] = np.array(self.cfg.robot.init_state.pos)
+        self.data.qpos[3:7] = np.array(self.cfg.robot.init_state.rot)
+        self.data.qpos[7:] = self.dof_init_pose
+        self.data.qvel[:]   = 0
+        
+        self.act[:] = 0
+        self.history_handler.reset([0])
+        self.timer: int = 0
+        
+        # self._subtimer = 0
+        
+        self.UpdateObs()
+        ...
+
+    @staticmethod
+    def pd_control(target_q, q, kp, target_dq, dq, kd):
+        '''Calculates torques from position commands
+        '''
+        if MujocoRobot.RAND_NOISE:
+            kp,kd = MujocoRobot.mk_rand_noise(np.array([kp, kd]), MujocoRobot.noise_ratio)
+        return (target_q - q) * kp + (target_dq - dq) * kd
+
+    def GetState(self):
+        '''Extracts physical states from the mujoco data structure
+        '''
+        data = self.data
+        self.q = data.qpos.astype(np.double)[7:] # 19 dim
+            # 3 dim base pos + 4 dim quat + 12 dim actuation angles
+        self.dq = data.qvel.astype(np.double)[6:] # 18 dim ?????
+            # 3 dim base vel + 3 dim omega + 12 dim actuation vel
+        
+        self.pos = data.qpos.astype(np.double)[:3]
+        self.quat = data.qpos.astype(np.double)[3:7]
+        self.vel = data.qvel.astype(np.double)[:3]
+        self.omega = data.qvel.astype(np.double)[3:6]
+        
+        r = R.from_quat(self.quat)
+        self.rpy = quaternion_to_euler_array(self.quat)
+        self.rpy[self.rpy > math.pi] -= 2 * math.pi
+        self.gvec = r.apply(np.array([0., 0., -1.]), inverse=True).astype(np.double)
+    
+        
+        if self.heading_cmd:
+            self.cmd[2] = np.clip(0.5*wrap_to_pi_float(self.cmd[3] - self.rpy[2]), -1., 1.)
+            
+        if self.is_motion_tracking:
+            self.motion_time = (self.timer) * self.dt 
+            self.ref_motion_phase = self.motion_time / self.motion_len
+            
+        # breakpoint()
+    
+        
+    def ApplyAction(self, action): 
+        self.timer+=1
+        if self.RAND_NOISE: action = MujocoRobot.mk_rand_noise(action, MujocoRobot.noise_ratio)
+        
+        self.act = action.copy()
+        target_q = np.clip(action, -self.clip_action_limit, self.clip_action_limit) * self.action_scale + self.dof_init_pose
+        
+        rand_mask = np.random.random(self.num_actions) < self.mask_ratio
+
+        rand_delay = np.random.randint((1e-3*self.delay_ratio[0])//self.sim_dt, (1e-3*self.delay_ratio[1])//self.sim_dt) * self.RAND_DELAY
+        step_delay = rand_delay//self.decimation
+        substep_delay = rand_delay - step_delay * self.decimation
+        
+        if step_delay ==0:
+            old_action = self.history_handler.history['actions'][0, step_delay]
+            old_trg_q = np.clip(old_action, -self.clip_action_limit, self.clip_action_limit) * self.action_scale + self.dof_init_pose
+            cur_trg_q = target_q
+        else:
+            old_action = self.history_handler.history['actions'][0, step_delay]
+            cur_action = self.history_handler.history['actions'][0, step_delay+1]
+            old_trg_q = np.clip(old_action, -self.clip_action_limit, self.clip_action_limit) * self.action_scale + self.dof_init_pose
+            cur_trg_q = np.clip(cur_action, -self.clip_action_limit, self.clip_action_limit) * self.action_scale + self.dof_init_pose
+                
+        for i in range(self.decimation):
+            self.GetState()
+            
+            if self.RAND_DELAY and i < substep_delay:
+                target_q = old_trg_q
+            elif self.RAND_MASK:
+                target_q = cur_trg_q * rand_mask + old_trg_q.numpy() * (1 - rand_mask)
+            else:
+                target_q = cur_trg_q
+                
+            tau = self.pd_control(target_q, self.q, self.kp,
+                            0, self.dq, self.kd)  # Calc torques
+            
+            if self.RAND_NOISE: tau = MujocoRobot.mk_rand_noise(tau, MujocoRobot.noise_ratio)
+            tau = np.clip(tau, -self.tau_limit, self.tau_limit)  # Clamp torques
+            
+            
+            # self.print_torque(tau)
+            # tau*=0
+            # print(np.linalg.norm(target_q-self.q), np.linalg.norm(self.dq), np.linalg.norm(tau))
+            # self.data.qpos[:3] = np.array([0,0,1])
+            self.data.ctrl[:] = tau
+
+            mujoco.mj_step(self.model, self.data) # type: ignore
+            
+            self.tracking()
+            if self.is_render:
+                if self.viewer.is_alive:
+                    self.viewer.render()
+                else:
+                    raise Exception("Mujoco Robot Exit")
+            # self._subtimer += 1
+        
+        self.UpdateObs()
+
+
+
+            
+        # breakpoint()
+
+    def Obs(self):
+        
+        # return {k: torch2np(v) for k, v in self.obs_buf_dict.items()}
+        
+        actor_obs = torch2np(self.obs_buf_dict['actor_obs']).reshape(1, -1)
+        if self.RAND_NOISE:
+            actor_obs = MujocoRobot.mk_rand_noise(actor_obs, MujocoRobot.noise_ratio)
+        return {
+            'actor_obs': actor_obs
+            }
+
+    def tracking(self):
+        if np.any(self.data.contact.pos[:,2] > 0.01):
+            names_list = self.model.names.decode('utf-8').split('\x00')[:40]
+            res = np.zeros((6,1),dtype=np.float64)
+            geom_name = lambda x: (names_list[self.model.geom_bodyid[x] + 1])
+            geom_force = lambda x:mujoco.mj_contactForce(self.model,self.data,x,res) #type:ignore
+            
+            for contact in self.data.contact:
+                if contact.pos[2] > 0.01 and contact.geom1 != 0 and contact.geom2 != 0:
+                    geom1_name = geom_name(contact.geom1)
+                    geom2_name = geom_name(contact.geom2)
+                    # logger.warning(f"Warning!!! Collision between '{geom1_name,contact.geom1}' and '{geom2_name,contact.geom2}' at position {contact.pos}.")
+                    
