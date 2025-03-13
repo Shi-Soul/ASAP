1397c2f3fd6e61a32fe261607b50865f396746b2
Shi-Soul <993520026@qq.com>
2025-03-13 20:15:08 +0800

    add g1 12dof, inertia

--- snippets.sh
+++ snippets.sh
@@ -47,8 +47,9 @@ robot.asset.urdf_file="g1/g1_23dof_lock_wrist.urdf" \
 num_envs=4096 \
 +device=cuda:0 \
 project_name=G1Loco \
-experiment_name=v1Nil_OPal_nostandstill \
+experiment_name=v1Nil_OPal3_nostandstill \
 rewards.reward_scales.standstill=0.0 \
+rewards.reward_scales.alive=3.0 \
 rewards.only_positive_rewards=True \
 headless=True
 
@@ -65,8 +66,9 @@ robot.asset.urdf_file="g1/g1_23dof_lock_wrist.urdf" \
 num_envs=4096 \
 +device=cuda:1 \
 project_name=G1Loco \
-experiment_name=v1drss_OPal_nostandstill \
+experiment_name=v1drss_OPal3_nostandstill \
 rewards.reward_scales.standstill=0.0 \
+rewards.reward_scales.alive=3.0 \
 rewards.only_positive_rewards=True \
 headless=True
 
@@ -83,10 +85,89 @@ robot.asset.urdf_file="g1/g1_23dof_lock_wrist.urdf" \
 num_envs=4096 \
 +device=cuda:2 \
 project_name=G1Loco \
-experiment_name=v1drss2_OPal_nostandstill \
+experiment_name=v1drss2_OPal3_nostandstill \
 rewards.reward_scales.standstill=0.0 \
+rewards.reward_scales.alive=3.0 \
 rewards.only_positive_rewards=True \
 headless=True
+
+###
+
+python humanoidverse/train_agent.py \
++simulator=isaacgym \
++exp=locomotion \
++terrain=terrain_locomotion_plane \
++robot=g1/g1_23dof_lock_wrist \
++domain_rand=dr_wjx_ss2 \
++rewards=loco/g1 \
++obs=loco/wjx_hist_dr \
+robot.asset.urdf_file="g1/g1_23dof_lock_wrist.urdf" \
+num_envs=4096 \
++device=cuda:2 \
+project_name=G1Loco \
+experiment_name=v1drss2_OPal1_nostandstill \
+rewards.reward_scales.standstill=0.0 \
+rewards.reward_scales.alive=1.0 \
+rewards.only_positive_rewards=True \
+headless=True
+
+
+python humanoidverse/train_agent.py \
++simulator=isaacgym \
++exp=locomotion \
++terrain=terrain_locomotion_plane \
++robot=g1/g1_23dof_lock_wrist \
++domain_rand=dr_wjx_ss2 \
++rewards=loco/g1 \
++obs=loco/wjx_hist_dr \
+robot.asset.urdf_file="g1/g1_23dof_lock_wrist.urdf" \
+num_envs=4096 \
++device=cuda:0 \
+project_name=G1Loco \
+experiment_name=v1drss2_OPal5_nostandstill \
+rewards.reward_scales.standstill=0.0 \
+rewards.reward_scales.alive=5.0 \
+rewards.only_positive_rewards=True \
+headless=True
+
+python humanoidverse/train_agent.py \
++simulator=isaacgym \
++exp=locomotion \
++terrain=terrain_locomotion_plane \
++robot=g1/g1_23dof_lock_wrist \
++domain_rand=dr_wjx_ss2 \
++rewards=loco/g1 \
++obs=loco/wjx_hist_dr \
+robot.asset.urdf_file="g1/g1_23dof_lock_wrist.urdf" \
+num_envs=4096 \
++device=cuda:1 \
+project_name=G1Loco \
+experiment_name=v1drss2_OPal10_nostandstill \
+rewards.reward_scales.standstill=0.0 \
+rewards.reward_scales.alive=10.0 \
+rewards.only_positive_rewards=True \
+headless=True
+
+### 
+
+python humanoidverse/train_agent.py \
++simulator=isaacgym \
++exp=locomotion \
++terrain=terrain_locomotion_plane \
++robot=g1/g1_23dof_lock_wrist \
++domain_rand=dr_wjx_ss2 \
++rewards=loco/g1 \
++obs=loco/wjx_hist_dr \
+robot.asset.urdf_file="g1/g1_23dof_lock_wrist.urdf" \
+num_envs=4096 \
++device=cuda:7 \
+project_name=G1Loco \
+experiment_name=v1drs_OPal5_nostandstill \
+rewards.reward_scales.standstill=0.0 \
+rewards.reward_scales.alive=5.0 \
+rewards.only_positive_rewards=True \
+headless=True
+
 
 # 25.03.12
 
